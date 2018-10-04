0006b7fba931266e902e46d3400c7e40ad8a8b2d
TebogoNkomondo <1124458@students.wits.ac.za>
2018-10-04 23:25:09 +0200

    passed bullet container by reference

--- game-source-code/collisionHandler.cpp
+++ game-source-code/collisionHandler.cpp
@@ -9,14 +9,21 @@ collisionHandler::~collisionHandler(){
 	
 }
 
-void collisionHandler::bulletSpider(Spider& spider1, BulletContainer& bulletStorage){
-	for(auto i = bulletStorage.getBulletVector().begin(); i<bulletStorage.getBulletVector().end();i++){
+tuple<bool, int> collisionHandler::bulletSpider(Spider& spider1, std::vector<Laser>& bulletStorage){
+	bool isBullet = false;
+	int ii=0;
+	for(auto i = bulletStorage.begin(); i<bulletStorage.end();i++,ii++){
 		sf::FloatRect collidingObject1 = (*i).getBullet().getGlobalBounds();
 		sf::FloatRect collidingObject2 = spider1.getSpider().getGlobalBounds();
+		isBullet = false;
 		if(collidingObject1.intersects(collidingObject2)){
 			spider1.deleteSpider();
-			(*i).deleteLaser();
+			isBullet = true;
+			bulletStorage.erase(i);
+			return {isBullet, ii};
 		}
+		return {false, 0};
+		//isBullet = false;
 	}
 }
 
@@ -29,6 +36,18 @@ bool collisionHandler::playerSpider(Spider& spider1, Player& player1){
 	return isPlayerDead;
 }
 
+void collisionHandler::bulletCentipede(std::vector<Laser>& bulletStorage1, std::vector<sf::Sprite>& centipedeStorage){
+	for(size_t i=0;i<bulletStorage1.size();i++){
+		for(size_t t=0;t<centipedeStorage.size();t++){
+			sf::FloatRect col1 = bulletStorage1.at(i).getBullet().getGlobalBounds();
+			sf::FloatRect col2 = centipedeStorage.at(t).getGlobalBounds();
+			if(col1.intersects(col2)){
+				centipedeStorage.at(t).setScale(0,0);
+				bulletStorage1.at(i).deleteLaser();
+			}
+		}
+	}
+}
 /*bool collisionHandler::centipedePlayer(Player& player1, CentipedeTrain& centipede1){
 	for(auto i = centipede1.getCentipedeVector().begin();i<centipede1.getCentipedeVector().end();i++){
 		sf::FloatRect collidingObject1 = (*i).getCentipedeSegment().getGlobalBounds();
--- game-source-code/collisionHandler.h
+++ game-source-code/collisionHandler.h
@@ -15,11 +15,11 @@ public:
 	
 	void bulletMushroom();
 	
-	void bulletSpider(Spider& spider1, BulletContainer& bulletStorage);
+	tuple<bool,int> bulletSpider(Spider& spider1, std::vector<Laser>& bulletStorage);
 	
 	bool playerSpider(Spider& spider1, Player& player1);
 	
-	//bool centipedePlayer(Player& player1, CentipedeTrain& centipede1);
+	void bulletCentipede(std::vector<Laser>& bulletStorage1, std::vector<sf::Sprite>& centipedeStorage);
 private:
 	bool isPlayerDead = false;
 	vector<Laser> bulletVec_;
